9e397219d93b7761c359865fb970e6e6e2277442
dmgdemos <dmgdemos@host.dmgvps.com>
2014-06-24 11:16:16 -0400

    phonegap build

---
+++ js/main.js
@@ -0,0 +1,839 @@
+
+
+/*
+
+                       A
+                       M
+                       M
+                       M
+                       M
+                       M
+                       M
+                       M
+                      /M\
+                     '[V]'
+                      [A]
+                     [,-']
+                     [/"\]
+                     / _ \
+                    / / | \
+                   / /_O_| \
+                  /______|__\
+                  |=_==_==_=|
+                  |  |   |  |
+                 V|  |.V.|__|V
+                 A|  |'A'| =|A
+                  |__|___|= |
+                  |__|___| =|
+                  |####|####|
+                 |    o|     |
+                 |     |     |
+                 |     |     |
+                |      |      |
+                |      |      |
+                |      |      |
+               |       |       |
+               |       |       |
+               |-------|-------|
+              |        |        |
+              |        |        |
+              |___.____|____.___|
+             |                   |
+             |___________________|
+            /|HH|      |HH][HHHHHI
+            [|##|      |##][#####I
+            [|##|      |#########I
+            [|##|______|#######m#I
+            [I|||||||||||||||||||I
+            [I|||||||||||||||||||I
+            [|                   |
+            [|    H  H          H|
+            [|    H  H          H|
+            [|    \hdF          V|
+            [|     `'            |
+            [|    d##b          d|
+            [|    #hn           #|
+            [|     ""#          }|
+            [|    \##/          V|
+            [|                   |
+            [|     dh           d|
+            [|    d/\h          d|
+            [|    H""H          H|
+            [|    "  "          "|
+            [|________.^.________|
+            [I########[ ]########I
+            [I###[]###[.]########I
+            [I###|||||[_]####||||I
+            [####II####|        n |
+           /###########|         " \
+           ############|           |
+          /############|            \
+          ######"######|            |
+         /             |####### #####\
+         |             |#######.######
+        /              |##############\
+        |              |###############
+       /_______________|###############\
+       I|||||||||||||||||||||||||||||||I
+       I|||||||||||||||||||||||||||||||I
+       I|||||||||||||||||||||||||||||||I
+       I|||||||||||||||||||||||||||||||I
+       |                               |
+       |-------------------------------|
+       |                               |
+       | [                  U          |
+       | [                  N          |
+       | !                  I          |
+       | [                  T          |
+       | [                  E          |
+       | }                  D          |
+       |                               |
+       |                               |
+       | {                  S          |
+       | [                  T          |
+       | :                  A          |
+       | [                  T          |
+       | [                  E          |
+      /| {  /|              S    |\    |
+     | |   | |                   | |   |
+     | |   | |                   | |   |
+     | |   | |                   | |   |
+     |_|___|_|___________________|_|___|
+     | |   | |                   | |   |\
+     | |___| |___________________| |___|]
+     | |###| |###################| |###|]
+     | |###| |###################| |###|]
+     | |###| |""""""""""#########| |"""|]
+     | |###| |         |#########| |   |]
+      \|####\|---------|#########|/----|]
+       |#####|         |#########|     |/
+       |#####|         |#########|     |
+      /]##### |        | ######## |    [\
+      []##### |        | ######## |    []
+      []##### |        | ######## |    []
+      []##### |        | ######## |    []
+      []##### |        | ######## |    []
+       |#####|---------|#########|-----|
+       |#####|         |#########|     |
+       |#####|         |##H######|     |
+       |#####|         |##H######|     |
+       |#####|         |##H######|     |
+       |#####|_________|##H######|_____|
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |     ####"""""""  H            |
+       |     ####"""""""  H            |
+       |     """""""""""  H            |
+       |     """""""""""  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |                  H            |
+       |__________________H____________|
+       |                  H            |
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       I||||||||||||||||||H||||||||||||I
+       |#####|         |##H######|     |
+       |#####|         |##H######|     |
+       |#####|  H   H  |##H######|   H |
+       |#####|  H   H  |##H######|   H |
+       |#####|  H   H  |##H######|   H |
+       |#####|  \h_dF  |##H######|   Vm|
+       |#####|   `"'   |##H######|    "|
+       |#####|         |##H######|     |
+       |#####|  /###\  |##H######|   /#|
+       |#####|  #   '  |##H######|   # |
+       |#####|  \###\  |##H######|   \#|
+       |#####|  .   #  |##H######|   . |
+       |#####|  \###/  |##H######|   \#|
+       |#####|         |##H######|     |
+       |#####|    H    |##H######|     [
+       |#####|   dAh   |##H######|    H|
+       |#####|  dF qL  |##H######|   dF|
+       |#####|  HhmdH  |##H######|   Hm|
+       |#####|  H   H  [%]H#apx##|   H |
+       |#####|         |##H######|     |
+       |#####A         |##H######A     |
+       |####| |        |##H#####|#|    |
+       |####| |        |##H#####|#|    |
+       |###|   |       |##H####|###|   |
+       |###|   |       |##H####|###|   |
+       |##|     |      |##H###|#####|  |
+       |#-|     |      |##H###|#####|-_|
+    _-"==|       |     |##H##|#######|=="-_
+ _-"=[]==|       |     |##H##|#######|==[]="-_
+|========|_______|_____|##H##|#######|========|
+!=======|=========|____|##H#|=========|=======!
+        !=========! /#####\ !=========!
+         /#######\ /#######\ /#######\
+        d#########V#########V#########h
+        H#########H#########H#########H
+       |###########H#######H###########|
+       |###########|"""""""|###########|
+        """""""""""         """""""""""
+
+               
+
+*/
+
+
+
+var name = "";
+var retries = 0;
+
+/* constants */
+var TAB_ID=null, TIMELINE_EVENT_ID=null;
+
+$(document).bind('mobileinit pageinit', function(e){
+
+	bindEvents();
+
+	//detect if user has entered a name
+	checkName();
+
+
+	updateContent();
+
+	//poll the server for updates
+	setInterval(updateContent, 5000);
+
+
+
+});
+
+
+$( document ).ready(function(){
+
+
+	/*
+
+		document is loaded
+
+	*/ 
+
+
+});
+
+
+
+
+function updateContent() {
+
+	//poll the server and update entire app content
+
+
+
+	/* Update Home Content */
+	 $.ajax({
+
+	     type: "GET",
+	     data: "id=" + appid,
+	     url: siteUrl + "getHomeContent.php"
+	 }).done(function(data) {
+
+	     document.getElementById('home-content').innerHTML = data;
+
+	 });
+
+	 
+	 //update event Timeline
+	 if(TAB_ID != null) {
+
+	 	$.ajax({
+
+	 		type: "GET",
+	 		data: "event_tab_id=" + TAB_ID,
+	 	  	url: siteUrl + "getEventTimeline.php"
+	 	}).done(function(data) {
+
+	 		document.getElementById('eventsTimelineContent').innerHTML = data;
+	 		document.getElementById('text-event-tab-id').value = TAB_ID;
+	 		document.getElementById('event_tab_id').value = TAB_ID;
+
+
+	 	});	
+
+	 }
+
+	 //update event Timeline Comments
+	 if(TIMELINE_EVENT_ID != null) {
+
+	 	$.ajax({
+
+	 		type: "GET",
+	 		data: "id=" + TIMELINE_EVENT_ID,
+	 	  	url: siteUrl + "getEventTimelineComments.php"
+	 	}).done(function(data) {
+
+	 		document.getElementById('eventTabCommentsContent').innerHTML = data;
+
+
+	 	});
+
+
+	 }
+	
+
+}
+
+function writeEventTimelineComment() {
+
+	//var timeline_event_id = document.getElementById('timeline-event-id').value;
+	var timeline_event_id = TIMELINE_EVENT_ID;
+
+	var comment = document.getElementById('timeline-event-comment').value;
+
+
+	if(comment == "") {
+		alert('must first enter a comment');
+	}
+	else {
+		
+			$.ajax({
+
+				type: "GET",
+				data: "timeline_event_id=" + timeline_event_id + "&comment=" + comment + "&name=" + name + "&appid="+appid,
+			  	url: siteUrl + "writeEventTimelineComment.php"
+			}).done(function(data) {
+
+				/*
+				document.getElementById('eventsTimelineContent').innerHTML = data;
+				document.getElementById('text-event-tab-id').value = event_tab_id;
+				document.getElementById('event_tab_id').value = event_tab_id;
+				$.mobile.navigate('#EventTimeline', { transition : "flow"});
+				*/
+
+				updateContent();
+				$( "#popupCommentBox" ).popup( "close" );
+
+
+			});
+	
+
+	}
+	
+}
+
+
+function getEventTimeline(event_tab_id) {
+
+	TAB_ID = event_tab_id;
+
+	$.ajax({
+
+		type: "GET",
+		data: "event_tab_id=" + event_tab_id,
+	  	url: siteUrl + "getEventTimeline.php"
+	}).done(function(data) {
+
+		document.getElementById('eventsTimelineContent').innerHTML = data;
+		document.getElementById('text-event-tab-id').value = event_tab_id;
+		document.getElementById('event_tab_id').value = event_tab_id;
+		$.mobile.navigate('#EventTimeline', { transition : "flow"});
+
+
+	});	
+
+}
+
+function getEventTimelineComments(id) {
+
+	TIMELINE_EVENT_ID = id;
+
+	$.ajax({
+
+		type: "GET",
+		data: "id=" + id,
+	  	url: siteUrl + "getEventTimelineComments.php"
+	}).done(function(data) {
+
+		/*
+		document.getElementById('eventsTimelineContent').innerHTML = data;
+		document.getElementById('text-event-tab-id').value = event_tab_id;
+		document.getElementById('event_tab_id').value = event_tab_id;
+		$.mobile.navigate('#EventTimeline', { transition : "flow"});
+		*/
+
+		document.getElementById('eventTabCommentsContent').innerHTML = data;
+		$.mobile.navigate('#EventTabComments', { transition : "flow"});
+
+
+	});
+
+	
+
+}
+
+function createTextPost() {
+
+	var event_tab_id = document.getElementById('text-event-tab-id').value;
+	var text = document.getElementById('event-text-box').value;
+
+
+	if(name === null) {
+
+		alert('You must enter a name');
+	}
+	else {
+
+			$.ajax({
+
+				type: "GET",
+				data: "event_tab_id=" + event_tab_id + '&text=' + text + '&name=' + name+"&appid="+appid,
+			  	url: siteUrl + "createTabTextPost.php"
+			}).done(function(data) {
+
+				getEventTimeline(event_tab_id);
+
+
+			});
+
+	}
+
+
+
+}
+
+function getEventPhotos(tab_id) {
+
+	$.ajax({
+
+		type: "GET",
+		data: "tab_id=" + tab_id,
+	  	url: siteUrl + "getEventPhotos.php"
+	}).done(function(data) {
+
+		writeHTML(data);
+		document.getElementById('event_tab_id').value = tab_id;
+
+	});	
+
+}
+
+
+function takePicture() {
+
+	var options = {
+	                    quality : 100,
+	                    destinationType : Camera.DestinationType.FILE_URI,
+	                    sourceType : Camera.PictureSourceType.CAMERA,
+	                    allowEdit : true,
+	                    encodingType: Camera.EncodingType.JPEG,
+	                    targetWidth: 500,
+	                    targetHeight: 500,
+	                    popoverOptions: CameraPopoverOptions,
+	                    saveToPhotoAlbum: false 
+	            };
+
+
+	navigator.camera.getPicture(onSuccess, onFail, options);
+
+
+}
+
+
+function onSuccess(fileURI) {
+
+		/*
+	    var image = document.getElementById('myImage');
+	    image.src = "data:image/jpeg;base64," + imageData;
+	    */
+	    var tab_id = document.getElementById('event_tab_id').value;
+	    var url = siteUrl + 'photoUpload.php';
+	    
+
+	    var win = function (r) {
+	            clearCache();
+	            retries = 0;
+	            console.log(r);
+	            getEventTimeline(tab_id);
+	        }
+	     
+	        var fail = function (error) {
+	        	console.log(error);
+	            if (retries == 0) {
+	                retries ++
+	                setTimeout(function() {
+	                    onSuccess(fileURI)
+	                }, 1000)
+	            } else {
+	                retries = 0;
+	                clearCache();
+	                alert('failed.');
+	            }
+	        }
+	     
+	        var options = new FileUploadOptions();
+	        options.fileKey = "file";
+	        options.fileName = fileURI.substr(fileURI.lastIndexOf('/') + 1);
+	        options.mimeType = "image/jpeg";
+	        //options.params = {}; // if we need to send parameters to the server request
+	        var params = new Object();
+	        params.appID = appid;
+	        params.eventTabID = document.getElementById('event_tab_id').value;
+	        params.name = name;
+	        options.params = params;
+	        var ft = new FileTransfer();
+	        ft.upload(fileURI, encodeURI(url), win, fail, options);
+	}
+
+function onFail(message) {
+	    document.getElementById('cameraError').innerHTML = message;
+}
+
+function clearCache() {
+    navigator.camera.cleanup();
+}
+
+
+function checkLogin() {
+
+	var pass = $('#LoginInputText').val();
+
+	$.ajax({
+		type: "GET",
+		data: "password=" + pass + "&appid=" + appid,
+	  	url: siteUrl + 'checkPassword.php'
+	}).done(function(data) {
+
+		if(data == 'success')
+			$.mobile.navigate('#Home', { transition : "flow"});
+		else
+			$( "#LoginPopup" ).popup( "open");
+
+	});
+
+}
+
+
+function checkName() {
+
+	var n = window.localStorage.getItem("name");
+	name = n;
+	var nameHtml = "";
+
+	if(n === null){
+
+		//name is unknown
+		nameHtml += "<center>";
+		nameHtml += "<h3>You have not entered your name yet. </h3>";
+		nameHtml += '<input type="text" id="nameInputText" placeholder="" />';
+			nameHtml += '<fieldset class="ui-grid-solo">';
+			  nameHtml += '<div class="ui-block-a"><button data-icon="flat-man" data-theme="a" onclick="javascript:updateName();">Submit</button></div>';
+			nameHtml += '</fieldset>';
+		nameHtml += "</center>";
+		nameHtml += "<br />";
+
+	}
+	else {
+
+		//name is in local Storage
+		nameHtml += "<h3>Welcome, " + n + "</h3>";
+
+	}
+
+	$('#name').html( nameHtml );
+
+}
+
+
+function updateName() {
+
+	var x = $('#nameInputText').val();
+
+	//set name
+	window.localStorage.setItem("name", x);
+	name = x;
+	checkName();
+
+}
+
+
+function getPhotos(album_id) {
+
+	//do ajax call
+	$.ajax({
+
+		type: "GET",
+		data: "id=" + album_id,
+	  	url: siteUrl + "getPhotos.php"
+	}).done(function(data) {
+
+		writeHTML(data);
+
+	});
+	
+}
+
+function writeHTML(data) {
+
+	var html = "";
+	html += data;
+	$('#gallery').html( html );
+	$("#gallery a").photoSwipe({ enableMouseWheel: false , enableKeyboard: false, allowUserZoom:false,captionAndToolbarAutoHideDelay:0 });
+	$.mobile.navigate( "#Photo", { transition : "flow"} );
+
+}
+
+
+function getEvents(app_id, event_id) {
+
+
+	//get event details and comments
+	$.ajax({
+
+		type: "GET",
+		data: "event_id=" + event_id,
+	  	url: siteUrl + "getEventDetails.php"
+	}).done(function(data) {
+
+		writeEventHTML(data, event_id);
+
+	});
+
+
+
+}
+
+function writeEventHTML(html, event_id) {
+	
+
+	//write out comments html
+	var x = "";
+	x += '<input type="search" id="eventComment" data-theme="b" value="Write Comment..." />';
+	x += '<fieldset class="ui-grid-solo">';
+	x += '<div class="ui-block-a"><a id="LoginButton" data-role="button" data-icon="flat-man" data-transition="flow" data-theme="a" onclick="javascript:writeEventComment(' + event_id + ',' + appid + ');">Comment</a></div>';
+	x += '</fieldset>';
+
+	html += x;
+
+	$('#event-content').html( html );
+	$('#Events').trigger('create');
+	$.mobile.navigate( "#Events", { transition : "flow"} );
+
+
+}
+
+
+function writeEventComment(event_id, app_id) {
+
+	var name  = localStorage.getItem("name");
+
+	var comment = $('#eventComment').val();
+	var x = $('#NoNamePopup');
+	var y = $('#NoCommentPopup');
+
+	if(name === null) {
+
+		x.popup("open");
+	}
+	else if(comment === "" || comment === "Write Comment...") {
+
+		y.popup("open");
+
+	}
+	else {
+
+		//write comment
+		$.ajax({
+
+			type: "GET",
+			data: "event_id=" + event_id + "&name=" + name + "&comment=" + comment + "&app_id=" + app_id,
+		  	url: siteUrl + "writeComment.php"
+		}).done(function(data) {
+
+			//writeHTML(data);
+			getEvents(app_id, event_id);
+
+		});
+	
+
+
+	}
+
+}
+
+
+
+function clearEventHtml() {
+
+
+
+}
+
+
+
+
+function bindEvents() {
+
+
+	//login authentication
+	$('#LoginButton').bind('touchstart mousedown', function() {
+
+
+		if(pass === $('#LoginInputText').val() )
+			$.mobile.navigate('#Home', { transition : "flow"});
+		else
+			$( "#LoginPopup" ).popup( "open");
+
+
+
+	}); //end of Login Bind()
+
+
+}
+
+
+
+
+/***
+
+
+	BUTTON EVENT LISTENERS
+
+
+***/
+
+
+function contactButtonClick() {
+
+	// find contacts
+    var options = new ContactFindOptions();
+    options.filter = "";      
+    options.multiple = true;  
+    filter = ["displayName"]; 
+
+	
+    navigator.contacts.find(filter, contactsSuccess, contactsError, options);
+    
+
+}
+
+
+function contactsSuccess(contacts) {
+
+
+	var html = "";
+	html += '<h1>Invite Contacts</h1>';
+	html += '<div data-role="fieldcontain">';
+	html += '<fieldset data-role="controlgroup">';
+
+	for (var i=0; i<contacts.length; i++) {
+
+	    html += '<input type="checkbox" name="checkbox-' + i + '" data-theme="c" id="checkbox-' + i + '"  />';
+        html += '<label for="checkbox-' + i + '" data-form="ui-btn-up-a">' + contacts[i].displayName + '</label>';
+	}
+
+	   html += ' </fieldset>';
+	html += '</div>';
+
+	document.getElementById('contacts-content').innerHTML = html;
+	$.mobile.navigate('#Contacts', { transition : "flow"});
+
+}
+
+function contactsError(contactError) {
+
+	alert('contact error: ' + contactError);
+
+}
+
+
+
+
+
+function showCreateNewPhotoCategory () {
+
+	$.mobile.navigate('#createNewPhotoCategory', { transition : "flow"});
+
+}
+
+function showCreateNewEvent () {
+
+	$.mobile.navigate('#createNewEvent', { transition : "flow"});
+
+}
+
+
+function createEventClick() {
+
+	var title = $('#eventTitle').val();
+	var date = $('#eventDate').val();
+	var details = $('#eventDetails').val();
+
+	div = document.getElementById('eventsOverlayContent');
+
+	//validation
+	if(title == "") {
+
+		div.innerHHTML = 'Enter a Title';
+
+	}
+	else if(date == "") {
+
+		div.innerHHTML = 'Enter a Date';
+	}
+	else if(details == "") {
+
+		div.innerHHTML = 'Enter Details';
+	}
+	else {
+
+		//process event
+
+	}
+
+
+
+}
+
+
+/***
+
+	USER INTERFACE EVENT LISTENERS
+
+
+***/
+
+function showLoading () {
+
+
+	var html = '<div style="width:50px;height:50px;">';
+	html = '<img src="img/loading2.gif" />';
+	html += '</div>';
+
+
+	$.mobile.loading( 'show', {
+		//text: 'Logging In...',
+		textVisible: true,
+		theme: 'a',
+		html: html
+	});
+
+}
+
+function hideLoading() {
+
+	$.mobile.loading('hide');
+
+}
+
+
+
+
